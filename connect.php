f543133b1418c8e3330561c7e274306ca077149f
SimonMartineau <martineausimon20@gmail.com>
2025-01-03 17:04:44 +0000

    S: Added connect file for the database.

--- connect.php
+++ connect.php
@@ -1,3 +1,18 @@
 <?php
 
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "mybook_db";
+
+$connection = mysqli_connect($host, $username, $password, $db);
+
+$first_name = "a";
+$last_name = "b";
+// Column names then variable names.
+$query = "insert into users (userid, first_name, last_name, gender, email, password, url_address) values (1, '$first_name', '$last_name', 'a', 'a', 'a', 'a')";
+
+// Sends query to database
+mysqli_query($connection, $query);
+
 ?>
